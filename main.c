7b4942ad80ae4a77137c0e120cae9c7bf702490a
sanskardudhe09 <69566078+sanskardudhe09@users.noreply.github.com>
2022-10-30 16:56:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,9 @@
 #include "sub.h"
 int main()
 {
-    int num1, num2;
+    int num1 = 9;
+    int num2 = 2;
     int res;
-    printf("Input any two numbers separated by comma : ");
-    scanf("%d,%d", &num1, &num2);
     res = sub(num1, num2);
     printf("The difference of the given numbers : %d\n", res);
     return 0;
