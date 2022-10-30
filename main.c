a765608f5bd247c23e8a413a7121542c6f6c0559
sanskardudhe09 <69566078+sanskardudhe09@users.noreply.github.com>
2022-10-30 19:36:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "sub.h"
 int main()
 {
-    int num1 = 10;
-    int num2 = 5;
+    int num1 = 8;
+    int num2 = 2;
     int res;
     res = sub(num1, num2);
     printf("The difference of the given numbers : %d\n", res);
